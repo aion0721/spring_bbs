643479a384a20ab971f4b322914b2d9d8a9089ea
aion0721 <rudolf.aion@gmail.com>
2020-06-02 21:43:11 +0900

    Create Service, controller

---
+++ src/main/java/com/example/springbbs/Controller/MainRestController.java
@@ -0,0 +1,35 @@
+package com.example.springbbs.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.example.springbbs.Service.ItemService;
+import com.example.springbbs.Domain.Item;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MainRestController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping("list")
+    public List<Item> list(@ModelAttribute Item item) {
+        return itemService.findAll();
+    }
+
+    @PostMapping("add")
+    public Item create(@RequestBody Item item) {
+        itemService.save(item);
+        return item;
+
+    }
+
+}
